cccdf6f82195e02f304abbbad16e7ea95c065784
backofficeailson-web <backofficeailson@gmail.com>
2026-05-04 06:39:00 -0300

    logger.py

---
+++ src/core/src/utils/logger.py
@@ -0,0 +1,13 @@
+import logging
+from src.core.config import LOG_DIR
+
+LOG_FILE = os.path.join(LOG_DIR, "app.log")
+
+logging.basicConfig(
+    filename=LOG_FILE,
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+logger = logging.getLogger("fitcontrol")
